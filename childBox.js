2bc375200ec445b191ce7bf2b46613fadfc766f8
rocket3989 <rocket3989@gmail.com>
2019-03-06 02:09:30 -0600

    replaced all tabs with spaces

--- childBox.js
+++ childBox.js
@@ -1,79 +1,79 @@
 function childBox(posx,posy,index){
-	this.posx = posx;
-	this.posy = posy;
-	this.index = index;
-	this.value = 2;				//0 is X, 1 is O, 2 is unplayed
-	
-	this.valueSet = function(value){
-		this.value = value;
-	}
-	this.winTest = function(){ 	//this function check for wins- so it retruns -1,0,1
-		if (this.value == 1)
-			return 1;			//if O, return 1
-		if (this.value == 0)
-			return -1;			//if X, return -1
-		return 0;   			//unplayed, return 0
-	}
-		
-	this.show = function(){
-		push();
-			translate(posx+25,posy+25);			//move to the center of the box
-			if (this.value == 0){				//draw an X
-				rotate(QUARTER_PI);				
-				rect(-3,-20,6,40);
-				rect(-20,-3,40,6);
-				}
-			if (this.value == 1){				//draw an O
-				noFill();
-				stroke(0);
-				strokeWeight(8);
-				ellipse(0,0,30)
-				}
-		pop();
-	}
-		
-			
-	
-	this.hover = function(){	//this function is called when the parent is hovered over
-		if(mouseX > this.posx && mouseX < this.posx + 56 && mouseY > this.posy && mouseY < this.posy + 56 && this.value == 2){ //if it is being hovered / hasnt been played on
-			
-			if(boxes[index].value != 2){
-				boxes.forEach(box => {
-					if(box.value == 2)
-						box.hovered();
-				});
-			}
-			else
-				boxes[index].hovered();  // hover over the corresponding box
+    this.posx = posx;
+    this.posy = posy;
+    this.index = index;
+    this.value = 2;                //0 is X, 1 is O, 2 is unplayed
+    
+    this.valueSet = function(value){
+        this.value = value;
+    }
+    this.winTest = function(){     //this function check for wins- so it retruns -1,0,1
+        if (this.value == 1)
+            return 1;            //if O, return 1
+        if (this.value == 0)
+            return -1;            //if X, return -1
+        return 0;               //unplayed, return 0
+    }
 
-			push();
-				translate(posx+25,posy+25);
-				fill(0);
+    this.show = function(){
+        push();
+            translate(posx+25,posy+25);            //move to the center of the box
+            if (this.value == 0){                //draw an X
+                rotate(QUARTER_PI);                
+                rect(-3,-20,6,40);
+                rect(-20,-3,40,6);
+                }
+            if (this.value == 1){                //draw an O
+                noFill();
+                stroke(0);
+                strokeWeight(8);
+                ellipse(0,0,30)
+                }
+        pop();
+    }
+        
+            
+    
+    this.hover = function(){    //this function is called when the parent is hovered over
+        if(mouseX > this.posx && mouseX < this.posx + 56 && mouseY > this.posy && mouseY < this.posy + 56 && this.value == 2){ //if it is being hovered / hasnt been played on
+            
+            if(boxes[index].value != 2){
+                boxes.forEach(box => {
+                    if(box.value == 2)
+                        box.hovered();
+                });
+            }
+            else
+                boxes[index].hovered();  // hover over the corresponding box
 
-				if (!turn){				//draw the correct symbol for the turn
-					rotate(QUARTER_PI);
-					rect(-3,-20,6,40);
-					rect(-20,-3,40,6);
-				}
-				else{
-					noFill();
-					stroke(0);
-					strokeWeight(8);
-					ellipse(0,0,30)
-				}
-			pop();
-			return true;
-		}
-		return false;
-	}
-	this.clicked = function(click){		//check if the box is clickable
-		if(mouseX > this.posx && mouseX < this.posx + 56 && mouseY > this.posy && mouseY < this.posy + 56 && this.value == 2){
-			this.value = turn;			//assign it the value of the current turn 
-			turn = (turn + 1) % 2;		//increment turn
-			playable = this.index;		//assign which box is available to be played in
-			return true;				//was clicked
-		}
-		return false;					//not clicked
-	}
-	
+            push();
+                translate(posx+25,posy+25);
+                fill(0);
+
+                if (!turn){                //draw the correct symbol for the turn
+                    rotate(QUARTER_PI);
+                    rect(-3,-20,6,40);
+                    rect(-20,-3,40,6);
+                }
+                else{
+                    noFill();
+                    stroke(0);
+                    strokeWeight(8);
+                    ellipse(0,0,30)
+                }
+            pop();
+            return true;
+        }
+        return false;
+    }
+    this.clicked = function(click){        //check if the box is clickable
+        if(mouseX > this.posx && mouseX < this.posx + 56 && mouseY > this.posy && mouseY < this.posy + 56 && this.value == 2){
+            this.value = turn;            //assign it the value of the current turn 
+            turn = (turn + 1) % 2;        //increment turn
+            playable = this.index;        //assign which box is available to be played in
+            return true;                //was clicked
+        }
+        return false;                    //not clicked
+    }
+    
 }
--- sketch.js
+++ sketch.js
@@ -1,130 +1,130 @@
-var turn = 0;   		//0 is X, 1 is O, 2 is unplayed
-var win = 2; 			//0 is X won, 1 is O won, 3 is tie
+var turn = 0;                   //0 is X, 1 is O, 2 is unplayed
+var win = 2;                    //0 is X won, 1 is O won, 3 is tie
 var drawn = 0;
 var boxes = [];
 var playable = -1;
 
-function setup() {		//stuff that needs to run at startup
-	noCursor();
-	frameRate(60);
-	createCanvas(windowWidth, windowHeight);
-	noStroke();
-	fill(0);
-	
-	for(i = 0; i < 3; i++)
-		for(j = 0; j < 3; j++)
-			boxes.push(new parentBox(50+210*j, 50+210*i, 3*i+j));
+function setup() {              //stuff that needs to run at startup
+    noCursor();
+    frameRate(60);
+    createCanvas(windowWidth, windowHeight);
+    noStroke();
+    fill(0);
+    
+    for(i = 0; i < 3; i++)
+        for(j = 0; j < 3; j++)
+            boxes.push(new parentBox(50+210*j, 50+210*i, 3*i+j));
 }
 
 function windowResized(){
-	resizeCanvas(windowWidth, windowHeight);
+    resizeCanvas(windowWidth, windowHeight);
 }
 
 function mouseClicked(){
-	let update = false;		//see if anything changes on mouseclick
-	if (playable + 1)
-		update = update || boxes[playable].clicked();
+    let update = false;         //see if anything changes on mouseclick
+    if (playable + 1)
+        update = update || boxes[playable].clicked();
 
-	else{
-		boxes.forEach(box => {
-			box.clicked();
-		});
-	}
-	if(win != 2){
-		
-		boxes.forEach(box => {
-			box.reset();
-		});
-			
-		win = 2;
-		turn = 0;
-		drawn = 0;
-		playable = -1;
-	}
-	if (update){	//test for win
-		win = 3; 	//assume the game is drawn
+    else{
+        boxes.forEach(box => {
+            box.clicked();
+        });
+    }
+    if(win != 2){
+        
+        boxes.forEach(box => {
+            box.reset();
+        });
+            
+        win = 2;
+        turn = 0;
+        drawn = 0;
+        playable = -1;
+    }
+    if (update){            //test for win
+        win = 3;            //assume the game is drawn
 
-		boxes.forEach(box => {
-			if(box.value == 2)
-				win = 2;
-		});
+        boxes.forEach(box => {
+            if(box.value == 2)
+                win = 2;
+        });
 
-		for (i = 0; i < 3; i++){
-			if (abs(boxes[3*i].winTest() + boxes[3*i+1].winTest() + boxes[3*i+2].winTest()) > 2)
-				win = boxes[3*i].value;
-			if (abs(boxes[i].winTest() + boxes[i+3].winTest() + boxes[i+6].winTest()) > 2)
-				win = boxes[i].value;
-		}
-		if(abs(boxes[0].winTest() + boxes[4].winTest() + boxes[8].winTest()) > 2)
-			win = boxes[0].value;
-		if(abs(boxes[2].winTest() + boxes[4].winTest() + boxes[6].winTest()) > 2)
-			win = boxes[2].value;
-		if (drawn == 9)
-			win = 3;
-	}
-	
+        for (i = 0; i < 3; i++){
+            if (abs(boxes[3*i].winTest() + boxes[3*i+1].winTest() + boxes[3*i+2].winTest()) > 2)
+                win = boxes[3*i].value;
+            if (abs(boxes[i].winTest() + boxes[i+3].winTest() + boxes[i+6].winTest()) > 2)
+                win = boxes[i].value;
+        }
+        if(abs(boxes[0].winTest() + boxes[4].winTest() + boxes[8].winTest()) > 2)
+            win = boxes[0].value;
+        if(abs(boxes[2].winTest() + boxes[4].winTest() + boxes[6].winTest()) > 2)
+            win = boxes[2].value;
+        if (drawn == 9)
+            win = 3;
+    }
+    
 }
-	
+    
 function draw() {
 
-	background(255);
+    background(255);
 
-	push();
-		translate(360,360);
-		if (win == 0 || win == 3){
-			fill(0);
-			rotate(QUARTER_PI);
-			rect(-30,-320,67,640);
-			rect(-320,-30,640,67);
-		}
-		if(win == 1 || win == 3){
-			noFill();
-			stroke(0);
-			strokeWeight(40);
-			ellipse(0,0,426)
-		}
-	pop();
+    push();
+        translate(360,360);
+        if (win == 0 || win == 3){
+            fill(0);
+            rotate(QUARTER_PI);
+            rect(-30,-320,67,640);
+            rect(-320,-30,640,67);
+        }
+        if(win == 1 || win == 3){
+            noFill();
+            stroke(0);
+            strokeWeight(40);
+            ellipse(0,0,426)
+        }
+    pop();
 
-	if(win != 2){
-		return 0;
-	}
+    if(win != 2){
+        return 0;
+    }
 
-	rect(250,50,10,620);
-	rect(460,50,10,620);
-	rect(50,250,620,10);
-	rect(50,460,620,10);	
+    rect(250,50,10,620);
+    rect(460,50,10,620);
+    rect(50,250,620,10);
+    rect(50,460,620,10);    
 
-	let hovered = false;
-	if (playable + 1){
-		hovered = boxes[playable].hover();
-		boxes[playable].outlined();
-	}
-	else{
-		boxes.forEach(box => {
-			hovered = hovered || box.hover();
-		});
-	}
-			
+    let hovered = false;
+    if (playable + 1){
+        hovered = boxes[playable].hover();
+        boxes[playable].outlined();
+    }
+    else{
+        boxes.forEach(box => {
+            hovered = hovered || box.hover();
+        });
+    }
+            
 
-	boxes.forEach(box => {
-		box.show();
-	})
-		
-	
-	if(!hovered){
-		push();
-			translate(mouseX,mouseY);
-			if (!turn){
-				rotate(QUARTER_PI);
-				rect(-3,-20,6,40);
-				rect(-20,-3,40,6);
-			}
-			else{
-				noFill();
-				stroke(0);
-				strokeWeight(8);
-				ellipse(0,0,30)
-				}
-		pop();
-	}
+    boxes.forEach(box => {
+        box.show();
+    })
+        
+    
+    if(!hovered){
+        push();
+            translate(mouseX,mouseY);
+            if (!turn){
+                rotate(QUARTER_PI);
+                rect(-3,-20,6,40);
+                rect(-20,-3,40,6);
+            }
+            else{
+                noFill();
+                stroke(0);
+                strokeWeight(8);
+                ellipse(0,0,30)
+                }
+        pop();
+    }
 }
